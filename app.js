31d26badd5b8ef012e1b04004e82ea8a6b948b1a
HarrisLeow <leow_harris@hotmail.sg>
2016-09-05 22:18:58 -0700

    fixing heroku bug

--- app.js
+++ app.js
@@ -17,13 +17,11 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
 // enable cors for all routes
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, User-Email, Auth-Token, Authorization')
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT')
-
   next()
 })
 
